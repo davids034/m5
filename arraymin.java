f3ff50f6ec4cc98df8767406fa3515dd19543de6
davids034 <96173668+davids034@users.noreply.github.com>
2024-01-17 23:30:23 +0100

    Add files via upload

---
+++ arraymin.java
@@ -0,0 +1,16 @@
+public class arraymin {
+    public static void main(String[] args) throws Exception {
+        double[] reales = {5.3, 2.7, 8.1, 3.5, 1.9, 1.2, 9.8, 4.6, 7.0, 2.1, 6.4};
+        double min = Double.MAX_VALUE;
+        double res = 0;
+
+        for (int i = 0; i < reales.length; i++) {
+            System.out.println(reales[i]);
+            if (reales[i] < min) {
+                min = reales[i];
+                res = min;
+            }
+        }
+        System.out.println("El valor mínimo es "+res);
+    }
+}
