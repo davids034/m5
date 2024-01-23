2ba73ea479997c09466bc00cb7b62975b102ff61
D a v i d s <dcarvajal21@ilg.cat>
2024-01-24 00:04:17 +0100

    desarrollo miembro b

--- arraymin.java
+++ arraymin.java
@@ -15,7 +15,7 @@ public class arraymin {
 <<<<<<< HEAD
         System.out.println("Prueba main error");
 =======
-        System.out.println("Prueba miembroB");
+        System.out.println("prueba miembroB");
 >>>>>>> desarrollo
     }
 }
