61e6d9519c705c5ef450709b2e50c0a2e0eccde4
D a v i d s <dcarvajal21@ilg.cat>
2024-01-24 00:07:29 +0100

    me rindo 2

--- arraymin.java
+++ arraymin.java
@@ -2,7 +2,7 @@ public class arraymin {
     public static void main(String[] args) throws Exception {
         double[] reales = {5.3, 2.7, 8.1, 3.5, 1.9, 1.2, 9.8, 4.6, 7.0, 2.1, 6.4};
         double min = Double.MAX_VALUE;
-        double res = 0;
+        double res = 7;
 
         for (int i = 0; i < reales.length; i++) {
             System.out.println(reales[i]);
